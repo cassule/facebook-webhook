9a70f46de114965d9e28a102612f65d9bd5954bd
Josué Francisco <josuefrancisco2017@outlook.pt>
2020-08-11 16:17:12 +0100

    add request http

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
@@ -76,6 +77,22 @@ function callSendAPI(sender_psid, response) {
     },
     message: response,
   };
+
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
 }
 
 app.listen(process.env.PORT || 1337, () => console.log("listen"));
